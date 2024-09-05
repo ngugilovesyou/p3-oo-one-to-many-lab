66ae7c61c3ed1fa7f074067ca13dccbc2046aa05
samuelgitau <amsamgitau@gmail.com>
2024-09-05 17:11:28 +0300

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
-
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner=None) -> None:
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    @property    
+    def pet_type(self):
+        return self._pet_type 
+    @pet_type.setter
+    def pet_type(self, new_pet_type):
+        if new_pet_type in Pet.PET_TYPES:
+            self._pet_type=new_pet_type 
+        else:
+            raise Exception ('Not a valid pet type.') 
+    @property        
+    def owner(self):
+        return self._owner
+        
+    @owner.setter
+    def owner(self, owner):
+        if owner is None or isinstance(owner, Owner):  # Allow None as a valid value
+            self._owner = owner
+        else:
+            raise TypeError("Object is not of type Owner")  
+    
+         
 class Owner:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+    def pets(self):
+        return [pets for pets in Pet.all if pets.owner ==self]    
+    def add_pet(self, pet):
+        if isinstance (pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception("Input object is not of type Pet") 
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)   
